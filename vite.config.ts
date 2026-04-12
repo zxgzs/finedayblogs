d52ce1f98e664ba7c837c541b5e4da1f814ee6bc
fineday <838791148@qq.com>
2026-04-12 20:31:51 +0800

    打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
+    base: env.VITE_BASE_URL || '/finedayblogs/',
     plugins: [
       vue(),
       AutoImport({
