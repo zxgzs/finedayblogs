ff3d042bfe87d144e3a0a65f1d7de0458e30b2dc
fineday <838791148@qq.com>
2026-04-16 19:55:28 +0800

    model.json

--- src/App.vue
+++ src/App.vue
@@ -612,7 +612,7 @@ onMounted(() => {
 	window.addEventListener('resize', handleWindowResize)
 
 	if (window.OML2D) {
-		let models = ['live2dmodels/sry_yushi/sry_yushi.model3.json']
+		let models = ['HK416-2-destroy/model.json']
 		window.OML2D.loadOml2d({
 			menus: {},
 			models: models.map(model => {
